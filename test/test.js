bb5515150d38e710fa85c36a8cfc4bb013f20c17
Cam Pedersen <diffference@gmail.com>
2012-01-13 12:23:32 -0500

    [test] use people for context

--- test/test.js
+++ test/test.js
@@ -1,3 +1,17 @@
-//var euclidean = require('../');
-var a = [];
-var b = [];
+var euclidean = require('../');
+
+var Bob = {
+ 'Blue': 1,
+ 'Yellow': 0,
+ 'Green': 1
+};
+
+var Jane = {
+ 'Blue': 0,
+ 'Yellow': 1,
+ 'Green': 1
+};
+
+console.log(
+  euclidean.distance(Bob, Jane)
+);
